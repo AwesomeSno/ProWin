241edfe9c13e87f46242887ca0db5fd5eb67e8f6
Harinandan J V <awesome.sno@gmail.com>
2026-03-15 16:04:35 +0530

    Initial Commit

---
+++ ProWin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProWin
+//
+//  Created by Harinandan J V on 15/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProWin/ProWinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProWinApp.swift
+//  ProWin
+//
+//  Created by Harinandan J V on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ProWinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
